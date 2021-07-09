6e4aab5fa1038e1d76854b6954c83234f9b9bfda
adavies <arielpdavies@gmail.com>
2021-07-09 14:26:33 -0400

    finished up most of login/create account css

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -53,18 +53,20 @@ class SessionForm extends React.Component{
             return (
                 <div className="Auth-Container"> 
                     <div className="logo-auth">
-                        Logo
+                        <Link to='/'>
+                            <img height="100" src="https://lh3.googleusercontent.com/5gfxy4BELemetG70hP3lfs3YS_xWQMxKHwp-bKm5q2-_PyCBP6rEN-s5rt0LVUUP5_XH84YELyjaSrnXHsKdp3Cz3391mhROcuy5Ho_zG-BKEh-oQjG42j3DHPPJNthYZ9pYhS5sSw=w500-h315-p-k" />
+                        </Link>
                     </div>
                     <div className="whole-form">
                         <div className="login-form-container">
                             <form onSubmit={this.handleSubmit} className="login-form-box">
                                 <div className="login-form-content">
                                     <div className="login-form-header">
-                                        <p className='form-descriptor'> Login </p>
+                                        <p className='form-descriptor'> Sign-In </p>
                                         <li className="errors">{ this.renderErrors()}</li>
                                     </div>
                                     <div className="login-form-main">
-                                        <label> Username:
+                                        <label className="login-form-main-content"> Username:
                                             <input 
                                                 type="text"
                                                 value={this.state.username}
@@ -73,7 +75,7 @@ class SessionForm extends React.Component{
                                             />
                                         </label>
                                         <br />
-                                        <label> Password:
+                                        <label className="login-form-main-content"> Password:
                                             <input 
                                                 type="password"
                                                 value={this.state.password}
@@ -92,35 +94,113 @@ class SessionForm extends React.Component{
                             </form>
                         </div>
                     </div>
+                    <div className="switch-to-other-form">
+                            <p>New to FashionOn?</p>
+                            <div>
+                                <Link to="/signup"><button className="switchFormsButton">Create Your FashionOn Account</button> </Link>
+                            </div>
+                    </div>
+                    <div className="form-footer">
+                        <div className="form-footer-items">
+                            <p className="form-footer-item-ps">Conditions of Use</p>
+                            <p className="form-footer-item-ps">Privacy Notice</p>
+                            <p className="form-footer-item-ps">Help</p>
+                        </div>
+                    </div>
                 </div>)
         } else if (formType === 'signup') {
-            return (<div className="signup-form-container">
-                <form onSubmit={this.handleSubmit} className="signup-form-box">
-                    {this.renderErrors()}
-                    <p className='form-descriptor'> Sign Up Below ⬇️ </p>
-                    <div className="signup-form">
-                    <label>Username
-                        <input
-                            type="text"
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            className="signin-input"
-                        />
-                    </label>
-                    <br />
-                    <label>Password
-                            <input
-                            type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            className="signin-input"
-                        />
-                    </label>
-                        <br />
-                        <input type="submit" value={formType} className="submit-button"/>
+            return(
+            <div className="Auth-Container">
+                    <div className="logo-auth">
+                        <Link to='/'>
+                            <img height="100" src="https://lh3.googleusercontent.com/5gfxy4BELemetG70hP3lfs3YS_xWQMxKHwp-bKm5q2-_PyCBP6rEN-s5rt0LVUUP5_XH84YELyjaSrnXHsKdp3Cz3391mhROcuy5Ho_zG-BKEh-oQjG42j3DHPPJNthYZ9pYhS5sSw=w500-h315-p-k" />
+                        </Link>
                     </div>
-                </form>
-            </div>)
+                    <div className="whole-form">
+                        <div className="login-form-container">
+                            <form onSubmit={this.handleSubmit} className="login-form-box">
+                                <div className="login-form-content">
+                                    <div className="login-form-header">
+                                        <p className='form-descriptor'> Create Account </p>
+                                        <li className="errors">{this.renderErrors()}</li>
+                                    </div>
+                                    <div className="login-form-main">
+                                        <label className="login-form-main-content"> Username:
+                                            <input
+                                                type="text"
+                                                value={this.state.username}
+                                                onChange={this.update('username')}
+                                                className="login-input"
+                                            />
+                                        </label>
+                                        <br />
+                                        <label className="login-form-main-content"> Password:
+                                            <input
+                                                type="password"
+                                                value={this.state.password}
+                                                onChange={this.update('password')}
+                                                className="login-input"
+                                            />
+                                        </label>
+                                        <br />
+                                        <div className="login-form-footer">
+                                            <input type="submit" value={formType} className="submit-button" />
+                                            <br />
+                                            {/* <button className="demoUser" onClick={this.handleDemoUser}>Demo User</button> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="switch-to-other-form">
+                        <p>Already have an account?</p>
+                        <div>
+                            <Link to="/login"><button className="switchFormsButton">Login To Your FashionOn Account</button> </Link>
+                        </div>
+                    </div>
+                    <div className="form-footer">
+                        <div className="form-footer-items">
+                            <p className="form-footer-item-ps">Conditions of Use</p>
+                            <p className="form-footer-item-ps">Privacy Notice</p>
+                            <p className="form-footer-item-ps">Help</p>
+                        </div>
+                    </div>
+                </div>)
+            
+            
+            
+            
+            
+            
+            
+            // return (<div className="signup-form-container">
+            //     <form onSubmit={this.handleSubmit} className="signup-form-box">
+            //         {this.renderErrors()}
+            //         <p className='form-descriptor'> Create Account  </p>
+            //         <div className="signup-form">
+            //         <label>Username
+            //             <input
+            //                 type="text"
+            //                 value={this.state.username}
+            //                 onChange={this.update('username')}
+            //                 className="signin-input"
+            //             />
+            //         </label>
+            //         <br />
+            //         <label>Password
+            //                 <input
+            //                 type="password"
+            //                 value={this.state.password}
+            //                 onChange={this.update('password')}
+            //                 className="signin-input"
+            //             />
+            //         </label>
+            //             <br />
+            //             <input type="submit" value={formType} className="submit-button"/>
+            //         </div>
+            //     </form>
+            // </div>)
         }  
     }
 }
