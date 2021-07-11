5e4bba0556252c4efbba13ec12a8e6e8936b4f91
adavies <arielpdavies@gmail.com>
2021-07-11 16:43:18 -0400

    added scroll to the top functionality

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function footer() {
-    return (
-<div className="footer">
-    <div>
-        Back to top
-    </div>
-    <div className="bigFooter">
-        <div>
-            <p className="footerInfo">
-                Get to know us
-            </p>
-        </div>
-        <div>
-            <p className="footerInfo">
-                Make Money with us
-            </p>
-        </div>
-        <div>
-            <p className="footerInfo">
-                FashionOn Payment Products
-            </p>
-        </div>
-        <div>
-            <p className="footerInfo">
-                Let us Help You
-            </p>
-        </div>
-        <div>
-            <p className="footerInfo">
-                Logo and locations
-            </p>
-        </div>
-    </div>
-</div>
-    )
+class Footer extends React.Component {
+        constructor(props){
+            super(props)
+            this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        }
+
+        componentDidUpdate(prevProps){
+            return window.scrollTo(0, 0);
+        }
+
+        render() {
+                return (<div className="footer">
+                        <div onClick={this.componentDidUpdate}>
+                            Back to top
+                        </div>
+                    <div className="bigFooter">
+                        <div>
+                            <p className="footerInfo">
+                                Get to know us
+                            </p>
+                        </div>
+                        <div>
+                            <p className="footerInfo">
+                                Make Money with us
+                            </p>
+                        </div>
+                        <div>
+                            <p className="footerInfo">
+                                FashionOn Payment Products
+                            </p>
+                        </div>
+                        <div>
+                            <p className="footerInfo">
+                                Let us Help You
+                            </p>
+                        </div>
+                        <div>
+                            <p className="footerInfo">
+                                Logo and locations
+                            </p>
+                        </div>
+                    </div>
+                </div>)
+
+            }
 };
 
-export default footer
+export default Footer
+
+
